fae5c9c34013a21873b99c62ec045d8f8ae4f158
Tim <35668555+twiley6@users.noreply.github.com>
2018-02-06 20:42:31 -0700

    Add files via upload

--- twileybh_cst236db.sql
+++ twileybh_cst236db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 06, 2018 at 03:03 PM
+-- Generation Time: Feb 06, 2018 at 08:41 PM
 -- Server version: 5.6.35
 -- PHP Version: 5.6.30
 
@@ -55,7 +55,9 @@ CREATE TABLE `products` (
   `name` varchar(45) NOT NULL,
   `description` varchar(200) NOT NULL,
   `stock` int(11) NOT NULL,
-  `price` decimal(10,0) NOT NULL,
+  `price` decimal(8,2) NOT NULL,
+  `unit` varchar(25) NOT NULL,
+  `tax_percentage` decimal(4,2) DEFAULT NULL,
   `fk_categoryID` int(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -70,6 +72,44 @@ CREATE TABLE `role` (
   `role` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`role_id`, `role`) VALUES
+(1, 'customer'),
+(2, 'administrator');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_item`
+--
+
+CREATE TABLE `sale_item` (
+  `id` int(10) NOT NULL,
+  `qty_sold` decimal(8,2) NOT NULL,
+  `price_per_unit` decimal(8,2) NOT NULL,
+  `total_price` decimal(8,2) NOT NULL,
+  `tax_amount` decimal(8,2) NOT NULL,
+  `transaction_id` int(10) NOT NULL,
+  `productID` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `transaction_id` int(10) NOT NULL,
+  `date_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `sale_amount` decimal(10,0) NOT NULL,
+  `tax_amount` decimal(10,0) DEFAULT NULL,
+  `fk_userName` varchar(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 --
@@ -82,8 +122,7 @@ CREATE TABLE `users` (
   `password` varchar(60) NOT NULL,
   `role_id` int(10) NOT NULL,
   `address` varchar(200) NOT NULL,
-  `acct_number` int(20) NOT NULL,
-  `transaction_id` int(10) NOT NULL
+  `acct_number` int(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -115,6 +154,18 @@ ALTER TABLE `role`
   ADD PRIMARY KEY (`role_id`);
 
 --
+-- Indexes for table `sale_item`
+--
+ALTER TABLE `sale_item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`transaction_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -143,7 +194,17 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `role_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `role_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `sale_item`
+--
+ALTER TABLE `sale_item`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `transaction_id` int(10) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
